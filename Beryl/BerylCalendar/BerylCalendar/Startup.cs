ea69cd240838bbc687283903d9c0e8a3c568e6a8
HaydenOrn <horn19@mail.wou.edu>
2021-03-04 15:13:40 -0800

    Fixed it working on azure

--- Beryl/BerylCalendar/BerylCalendar/Startup.cs
+++ Beryl/BerylCalendar/BerylCalendar/Startup.cs
@@ -12,6 +12,7 @@ using BerylCalendar.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Authorization;
 
 namespace BerylCalendar
 {
@@ -27,9 +28,14 @@ namespace BerylCalendar
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //services.AddDbContext<ApplicationDbContext>(options =>
+                //options.UseSqlServer(
+                    //Configuration.GetConnectionString("BerylConnection")));
+            //services.AddDatabaseDeveloperPageExceptionFilter();
+
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
-                    Configuration.GetConnectionString("BerylConnection")));
+                    Configuration.GetConnectionString("BerylCalendarAzureString")));
             services.AddDatabaseDeveloperPageExceptionFilter();
 
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
